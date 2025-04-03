c0e0332df245da3eeb6bca33126ea0b7d034f55b
JGPare <jules.g.pare@gmail.com>
2025-04-02 19:42:53 -0700

    adding in some plugins

--- canada-climate-data/src/App/Utility/leafletPlugin.js
+++ canada-climate-data/src/App/Utility/leafletPlugin.js
@@ -3,16 +3,16 @@
 import L from 'leaflet/dist/leaflet.js';
 
 // // plugins
-// import './mapsrc/plugins/L.Control.Pan.js'
-// import './mapsrc/plugins/L.Control.MousePosition.js'
-// import './mapsrc/plugins/L.Control.Zoomslider.js'
+import './mapsrc/plugins/L.Control.Pan.js'
+import './mapsrc/plugins/L.Control.MousePosition.js'
+import './mapsrc/plugins/L.Control.Zoomslider.js'
 // import './mapsrc/plugins/Leaflet.PolylineMeasure.js'
 
 // // styles
-// import './mapsrc/plugins/L.Control.Pan.css'
-// import './mapsrc/plugins/L.Control.MousePosition.css'
-// import './mapsrc/plugins/L.Control.Zoomslider.css'
+import './mapsrc/plugins/L.Control.Pan.css'
+import './mapsrc/plugins/L.Control.MousePosition.css'
+import './mapsrc/plugins/L.Control.Zoomslider.css'
 // import './mapsrc/plugins/Leaflet.PolylineMeasure.css'
-// import './mapsrc/leaflet.css'
+import './mapsrc/leaflet.css'
 
 export default L;
